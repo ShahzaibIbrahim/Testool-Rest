a248c09a0d3d4c9153377e1b3282d567517c1950
Shahzaib Ibrahim <shahzaibibrahim96@gmail.com>
2020-12-24 10:45:49 +0500

    Initial commit

---
+++ src/main/java/com/webster/rest/TestoolRest/TestoolRestApplication.java
@@ -0,0 +1,13 @@
+package com.webster.rest.TestoolRest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestoolRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestoolRestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/webster/rest/TestoolRest/TestoolRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.webster.rest.TestoolRest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestoolRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
